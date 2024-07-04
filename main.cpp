9ab822e70b6f1d8e44a8872aab8747656b09149c
Dipper209 <3611691282@qq.com>
2024-07-04 09:28:48 +0800

    对接服务器，完成登录对接，cookie读取。

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    User test("11","22");
+    //测试用户
+    User test("newuser@example.com","123456");
     test.login();
 
     MainWindow w;
--- user.h
+++ user.h
@@ -9,6 +9,8 @@
 #include <QJsonObject>
 #include <QUrl>
 #include <QDebug>
+#include <QNetworkCookie>
+#include <QNetworkCookieJar>
 
 
 class User : public QObject {
@@ -16,10 +18,11 @@ class User : public QObject {
 
 private:
     QString account;
-    QString token;
+    QString data;
     QString hashedPassword;
     QNetworkAccessManager *manager;
     QNetworkReply *reply;
+    QString session;
 public:
     User(QObject *parent = nullptr) : QObject(parent) {
         manager = new QNetworkAccessManager(this);
@@ -33,18 +36,22 @@ public:
     }
     void login() {
         //https post a requests
-        QUrl url("https://www.mocklib.com/mock/public/auth/login");
+        QUrl url("https://syncapi.snakekiss.com/login");
         QNetworkRequest request(url);
-        QString data = QString("username=%1&password=%2").arg(this->account).arg(this->hashedPassword);
-        //reply = manager->post(request, data.toUtf8());
-        reply = manager->get(request);
+        // 设置请求头
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+        request.setHeader(QNetworkRequest::UserAgentHeader, "MyApp/1.0");
+        //请求data
+        QString data = QString("email=%1&password=%2").arg(this->account).arg(this->hashedPassword);
+        reply = manager->post(request, data.toUtf8());
+        //reply = manager->get(request);
     }
     QString getUserHash() {
         QByteArray hash = QCryptographicHash::hash(account.toUtf8(), QCryptographicHash::Sha1);
         return hash.toHex(); // 将散列值转换为十六进制字符串并返回
     }
 signals:
-    void loginCompleted(const QString &token);//登陆完成的信号
+    void loginCompleted(const QString &session);//登陆完成的信号
 private slots:
     void onFinished() {
         if (reply->error() == QNetworkReply::NoError) {
@@ -59,16 +66,34 @@ private slots:
                 if (jsonObject.contains("message")) {
                     qDebug() << "Value for message: " << jsonObject.value("message").toString();
                 }
-                if (jsonObject.contains("token")) {
-                    qDebug() << "Value for token: " << jsonObject.value("token").toString();
-                    this->token = jsonObject.value("token").toString();
-                    emit loginCompleted(token); // 发射信号,ui方建立connect进行接收
+                if (jsonObject.contains("data")) {
+                    qDebug() << "Value for data: " << jsonObject.value("data").toString();
                 }
             } else {
                 qDebug() << "Invalid JSON format";
             }
         } else {
             qDebug() << "Error: " << reply->errorString();
+        }
+
+        // 通过 QNetworkAccessManager 获取 QNetworkCookieJar 并获取 cookies
+        QNetworkAccessManager *manager = dynamic_cast<QNetworkAccessManager*>(reply->parent());
+        if (manager) {
+            QNetworkCookieJar *cookieJar = manager->cookieJar();
+            QList<QNetworkCookie> cookies = cookieJar->cookiesForUrl(reply->url());
+            if (!cookies.isEmpty()) {
+                qDebug() << "Received cookies:";
+                for (const QNetworkCookie &cookie : cookies) {
+                    qDebug() << "Cookie Name:" << cookie.name();
+                    qDebug() << "Cookie Value:" << cookie.value();
+                    this->session=cookie.value();
+                    emit loginCompleted(session); // 发射信号,ui方建立connect进行接收
+                }
+            } else {
+                qDebug() << "No cookies received.";
+            }
+        } else {
+            qDebug() << "Failed to get QNetworkAccessManager.";
         }
 
         reply->deleteLater();
