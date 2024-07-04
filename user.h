3bb1cfc4801938e3f47854c0137e5c028d0fcda3
Dipper209 <3611691282@qq.com>
2024-07-04 11:05:55 +0800

    user.h美化代码，拆分功能使其格式化。

--- user.h
+++ user.h
@@ -2,16 +2,15 @@
 #define USER_H
 #include <QString>
 #include <QCryptographicHash>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
 #include <QJsonDocument>
 #include <QJsonObject>
-#include <QUrl>
-#include <QDebug>
+#include <QNetworkAccessManager>
 #include <QNetworkCookie>
 #include <QNetworkCookieJar>
-
+#include <QNetworkReply>
+#include <QObject>
+#include <QString>
+#include <QDebug>
 
 class User : public QObject {
     Q_OBJECT
@@ -20,72 +19,82 @@ private:
     QString account;
     QString data;
     QString hashedPassword;
-    QNetworkAccessManager *manager;
-    QNetworkReply *reply;
+    QNetworkAccessManager* manager;
+    QNetworkReply* reply;
     QString session;
+
 public:
-    User(QObject *parent = nullptr) : QObject(parent) {
-        manager = new QNetworkAccessManager(this);
-        connect(manager, &QNetworkAccessManager::finished, this, &User::loginonFinished);
+    explicit User(QObject* parent = nullptr) : QObject(parent), manager(new QNetworkAccessManager(this)) {
+        connect(manager, &QNetworkAccessManager::finished, this, &User::loginFinished);
     }
-    User(QString account, QString password, QObject *parent = nullptr) : QObject(parent) {
-        this->account = account;
-        this->hashedPassword = password;
-        manager = new QNetworkAccessManager(this);
-        connect(manager, &QNetworkAccessManager::finished, this, &User::loginonFinished);
+
+    User(const QString& account, const QString& password, QObject* parent = nullptr) : QObject(parent), account(account), hashedPassword(password), manager(new QNetworkAccessManager(this)) {
+        connect(manager, &QNetworkAccessManager::finished, this, &User::loginFinished);
     }
+
     void login() {
-        //https post a requests
         QUrl url("https://syncapi.snakekiss.com/login");
         QNetworkRequest request(url);
-        // 设置请求头
+
         request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
         request.setHeader(QNetworkRequest::UserAgentHeader, "MyApp/1.0");
-        //请求data
-        QString data = QString("email=%1&password=%2").arg(this->account).arg(this->hashedPassword);
-        reply = manager->post(request, data.toUtf8());
+
+        QString postData = QString("email=%1&password=%2").arg(account, hashedPassword);
+        reply = manager->post(request, postData.toUtf8());
     }
-    QString getUserHash() {
+
+    QString getUserHash() const {
         QByteArray hash = QCryptographicHash::hash(account.toUtf8(), QCryptographicHash::Sha1);
-        return hash.toHex(); // 将散列值转换为十六进制字符串并返回
+        return hash.toHex();
     }
+
 signals:
-    void loginCompleted(const QString &session);//登陆完成的信号
+    void loginCompleted(const QString& session);//登陆成功的信号，发送获取到的cookie
+
 private slots:
-    void loginonFinished() {
+    void loginFinished() {
         if (reply->error() == QNetworkReply::NoError) {
             QByteArray responseData = reply->readAll();
             QJsonDocument jsonDoc = QJsonDocument::fromJson(responseData);
 
             if (jsonDoc.isObject()) {
                 QJsonObject jsonObject = jsonDoc.object();
-                if (jsonObject.contains("code")) {
-                    qDebug() << "Value for code: " << jsonObject.value("code").toString();
-                }
-                if (jsonObject.contains("message")) {
-                    qDebug() << "Value for message: " << jsonObject.value("message").toString();
-                }
-                if (jsonObject.contains("data")) {
-                    qDebug() << "Value for data: " << jsonObject.value("data").toString();
-                }
+                processJsonObject(jsonObject);
             } else {
                 qDebug() << "Invalid JSON format";
             }
         } else {
-            qDebug() << "Error: " << reply->errorString();
+            qDebug() << "Error:" << reply->errorString();
         }
 
-        // 通过 QNetworkAccessManager 获取 QNetworkCookieJar 并获取 cookies
+        processCookies();
+        reply->deleteLater();
+    }
+
+private:
+    void processJsonObject(const QJsonObject& jsonObject) {
+        if (jsonObject.contains("code")) {
+            qDebug() << "Value for code:" << jsonObject.value("code").toString();
+        }
+        if (jsonObject.contains("message")) {
+            qDebug() << "Value for message:" << jsonObject.value("message").toString();
+        }
+        if (jsonObject.contains("data")) {
+            qDebug() << "Value for data:" << jsonObject.value("data").toString();
+        }
+    }
+
+    void processCookies() {
         if (manager) {
-            QNetworkCookieJar *cookieJar = manager->cookieJar();
+            QNetworkCookieJar* cookieJar = manager->cookieJar();
             QList<QNetworkCookie> cookies = cookieJar->cookiesForUrl(reply->url());
             if (!cookies.isEmpty()) {
                 qDebug() << "Received cookies:";
-                for (const QNetworkCookie &cookie : cookies) {
+                for (const QNetworkCookie& cookie : cookies) {
                     qDebug() << "Cookie Name:" << cookie.name();
                     qDebug() << "Cookie Value:" << cookie.value();
-                    this->session=cookie.value();
-                    emit loginCompleted(session); // 发射信号,ui方建立connect进行接收
+                    session = cookie.value();
+                    emit loginCompleted(session);
                 }
             } else {
                 qDebug() << "No cookies received.";
@@ -93,8 +102,6 @@ private slots:
         } else {
             qDebug() << "Failed to get QNetworkAccessManager.";
         }
-
-        reply->deleteLater();
     }
 };
 
