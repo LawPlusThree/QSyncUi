c183157bcb18fa5b33dd3bbffd137bb569631874
Alexander-Porter <alexanderporter@126.com>
2024-07-05 08:51:10 +0800

    Recover mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "ElaContentDialog.h"
+#include "ElaWindow.h"
+class HomePage;
+class T_Icon;
+class T_ElaScreen;
+class T_BaseComponents;
+class T_TabWidget;
+class MainWindow : public ElaWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    Q_SLOT void onCloseButtonClicked();
+
+
+private:
+    HomePage *_homePage{nullptr};
+    T_ElaScreen *_elaScreenPage{nullptr};
+    T_Icon *_iconPage{nullptr};
+    T_BaseComponents *_baseComponentsPage{nullptr};
+    T_TabWidget *_tabWidgetPage{nullptr};
+    QString _elaDxgiKey{""};
+    QString _aboutKey{""};
+    QString _settingKey{""};
+};
+#endif // MAINWINDOW_H
