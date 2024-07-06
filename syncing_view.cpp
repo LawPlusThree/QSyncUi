a49236b25c7125d91b1d4e7faca4b3e09b12d53c
kuiling <3110428854@qq.com>
2024-07-06 16:03:32 +0800

    完成了正在同步界面的上方ui设计

--- syncing_view.cpp
+++ syncing_view.cpp
@@ -1,5 +1,7 @@
 #include "syncing_view.h"
 #include "ElaText.h"
+#include "ElaPushButton.h"
+#include "ElaToggleButton.h"
 #include <QVBoxLayout>
 
 SyncingPage::SyncingPage(QWidget* parent)
@@ -10,9 +12,6 @@ SyncingPage::SyncingPage(QWidget* parent)
     centerVLayout->setContentsMargins(0, 0, 5, 0); // 设置布局边距,5为到右边的距离
     centralWidget->setWindowTitle("正在同步"); // 设置窗口标题
 
-    ElaText* iconTitle = new ElaText("On this page, you can use the search function to use your favorite icons in the program", this);
-    iconTitle->setTextSize(15); // 设置文字大小
-
     // 创建一个 ElaProgressBar 对象，用作进度条
     _progressBar = new ElaProgressBar(this);
     // 设置QProgressBar的大小策略为水平方向可扩展，垂直方向固定
@@ -22,16 +21,11 @@ SyncingPage::SyncingPage(QWidget* parent)
     // 设置进度条的最小值和最大值
     _progressBar->setMinimum(0);
     _progressBar->setMaximum(0); // 表示不确定的进度
-
-    ElaScrollPageArea* upBarArea = new ElaScrollPageArea(this);//上方固定区域
-    QVBoxLayout* upBarLayout = new QVBoxLayout(upBarArea);//上方固定区域垂直布局
-    upBarLayout->setContentsMargins(0, 0, 0, 0);//设置布局的边距
-
     // 创建一个 ElaScrollPageArea 对象，用作进度条的容器
-    ElaScrollPageArea* progressBarArea = new ElaScrollPageArea(upBarArea);
+    ElaScrollPageArea* progressBarArea = new ElaScrollPageArea(this);
     // 为进度条容器设置水平布局
     QHBoxLayout* progressBarLayout = new QHBoxLayout(progressBarArea);
-    progressBarLayout->setContentsMargins(50, 0, 50, 0); // 设置布局的边距
+    progressBarLayout->setContentsMargins(50, 0, 55, 0); // 设置布局的边距
     // 创建一个 ElaText 对象，用于显示进度条的文本标签
     ElaText* progressBarText = new ElaText("总进度：", this);
     // 设置进度条文本标签的字体大小为 15
@@ -40,14 +34,50 @@ SyncingPage::SyncingPage(QWidget* parent)
     progressBarLayout->addWidget(progressBarText);
     progressBarLayout->addWidget(_progressBar);
 
-    ElaText* catalogueText = new ElaText("这是目录", this);//创建一个ElaText对象，用于显示目录文本
-    catalogueText->setTextSize(15); // 设置文字大小
+    // 创建一个 ElaToggleButton 对象，设置其标签为 "Head" 并指定其父对象
+    _pushButton = new ElaPushButton("上传文件", this);
+    _pushButton->setFixedSize(100, 40); // 设置按钮的固定大小
+    _toggleButton = new ElaToggleButton("完成时提醒", this);
+    _toggleButton->setFixedSize(100, 40); // 设置按钮的固定大小
+    // 创建一个 ElaScrollPageArea 对象，用作切换按钮的容器，并指定其父对象
+    ElaScrollPageArea* pushButtonArea = new ElaScrollPageArea(this);
+    // 为切换按钮容器设置水平布局
+    QHBoxLayout* pushButtonLayout = new QHBoxLayout(pushButtonArea);
+    pushButtonLayout->setContentsMargins(50, 0, 55, 0);
+    // 将切换按钮控件添加到布局中
+    pushButtonLayout->addWidget(_pushButton);
+    pushButtonLayout->addWidget(_toggleButton);
+    // 在布局中添加一个弹性空间，使得所有控件靠左对齐
+    pushButtonLayout->addStretch();
 
-    upBarLayout->addWidget(progressBarArea);//将进度条容器添加到上方固定区域
-    upBarLayout->addWidget(catalogueText);//将目录文本添加到上方固定区域
+    ElaScrollPageArea* catalogueArea = new ElaScrollPageArea(this);
+    QHBoxLayout* catalogueLayout = new QHBoxLayout(catalogueArea);
+    catalogueLayout->setContentsMargins(10, 0, 15, 0);
+    ElaText* catalogueText1 = new ElaText("文件名", this);
+    catalogueText1->setTextSize(15);
+    catalogueText1->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    ElaText* catalogueText2 = new ElaText("数据大小", this);
+    catalogueText2->setTextSize(15);
+    catalogueText2->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    ElaText* catalogueText3 = new ElaText("网络速度", this);
+    catalogueText3->setTextSize(15);
+    catalogueText3->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    ElaText* catalogueText4 = new ElaText("同步状态", this);
+    catalogueText4->setTextSize(15);
+    catalogueText4->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    ElaText* catalogueText5 = new ElaText("操作", this);
+    catalogueText5->setTextSize(15);
+    catalogueText5->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    catalogueLayout->addWidget(catalogueText1);
+    catalogueLayout->addWidget(catalogueText2);
+    catalogueLayout->addWidget(catalogueText3);
+    catalogueLayout->addWidget(catalogueText4);
+    catalogueLayout->addWidget(catalogueText5);
 
-    centerVLayout->addWidget(upBarArea); // 将 progressBarArea 添加到布局中
-    centerVLayout->addWidget(iconTitle);// 将字添加到布局中
+    centerVLayout->addWidget(progressBarArea); // 将上方固定区域添加到布局中
+    centerVLayout->addWidget(pushButtonArea); // 将切换按钮容器添加到布局中
+    centerVLayout->addWidget(catalogueArea); // 将目录文本添加到布局中
+    centerVLayout->addStretch(); // 在布局的末尾添加一个弹性空间
 
     this->addCentralWidget(centralWidget); // 将中心部件添加到窗口中
 }
