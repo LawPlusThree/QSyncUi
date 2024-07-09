6b21185c3aa2f301f00c913bf948e43638832442
kuiling <3110428854@qq.com>
2024-07-09 12:18:13 +0800

    取消了链接新文件夹界面的最大化，最小化

--- setexcludeditems_view.cpp
+++ setexcludeditems_view.cpp
@@ -12,7 +12,7 @@ setExcludedItems_view::setExcludedItems_view(QWidget *parent)
     this->setWindowTitle("珞珈云"); // 设置窗口标题
     this->setWindowModality(Qt::ApplicationModal); // 设置窗口模态
     this->hide(); // 初始隐藏当前窗口
-    this->setWindowFlags(Qt::WindowMinimizeButtonHint); // 设置窗口标志，允许最小化
+    this->setWindowButtonFlag(ElaAppBarType::StayTopButtonHint,false);
     QWidget*centerarea=new QWidget(); // 创建一个新的QWidget对象作为主区域
     QVBoxLayout* centerVLayout = new QVBoxLayout(centerarea); // 为中心部件设置垂直布局
     centerVLayout->setContentsMargins(60, 15, 60, 30); // 设置布局边距
