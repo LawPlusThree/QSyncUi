61ce49695161c4017d2de544cdb98cd41002f567
kuiling <3110428854@qq.com>
2024-07-10 19:22:25 +0800

    终于对齐主页面的ui了   哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈

--- filecard.cpp
+++ filecard.cpp
@@ -6,57 +6,79 @@
 #include"ElaProgressBar.h"
 FileCard::FileCard(QString f, QString d,QString s,QString p)
 {
-    filename=new ElaText(f);
-    datasize=new ElaText(d);
-    speed=new ElaText(s);
-    progress=new ElaText(p);
+    // 创建ElaText对象来显示文件名，而不是直接在_checkBox中显示
+    filename = new ElaText(f, this);
+    datasize = new ElaText(d, this);
+    speed = new ElaText(s, this);
+    progress = new ElaText(p, this);
 
-    _checkBox = new ElaCheckBox(filename->text(), this);
-    QVBoxLayout*checkBoxArea=new QVBoxLayout();
-    checkBoxArea->addWidget(_checkBox,0,Qt::AlignCenter);
+    filename->setWordWrap(false);//禁止换行
+    datasize->setWordWrap(false);
+    speed->setWordWrap(false);
+    progress->setWordWrap(false);
+
+    datasize->setAlignment(Qt::AlignCenter);
+    speed->setAlignment(Qt::AlignCenter);
+    progress->setAlignment(Qt::AlignCenter);
+
+    // 创建一个没有文字的_checkBox
+    _checkBox = new ElaCheckBox("", this);
+    _checkBox->setFixedSize(25, 25);
+    QHBoxLayout* checkBoxArea = new QHBoxLayout();
+    checkBoxArea->addWidget(_checkBox, 0, Qt::AlignCenter); // 将_checkBox放在布局的最左侧
+
+    // 设置文件名文字的布局
+    filename->setTextSize(16);
+    filename->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    QHBoxLayout* filenameArea = new QHBoxLayout();
+    filenameArea->addWidget(filename, 0, Qt::AlignLeft); // 将文件名放在左侧
 
     datasize->setTextSize(16);
-    QVBoxLayout*dataSizeArea=new QVBoxLayout();
-    dataSizeArea->addWidget(datasize,0,Qt::AlignCenter);
-
-    speed->setTextSize(16);
-    QVBoxLayout*speedArea=new QVBoxLayout();
-    speedArea->addWidget(speed,0,Qt::AlignCenter);
+    datasize->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    datasize->setFixedSize(100,20);
+    QVBoxLayout* dataSizeArea = new QVBoxLayout();
+    dataSizeArea->addWidget(datasize, 0, Qt::AlignCenter);
+    //dataSizeArea->SetFixedSize(50, 20);
 
     proBar = new ElaProgressBar(this);
     proBar->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-    proBar->setFixedSize(100,15);
+    proBar->setFixedSize(140, 20);
     proBar->setMinimum(0);
     proBar->setMaximum(0);
     progress->setTextSize(16);
-    QVBoxLayout*proBarArea=new QVBoxLayout();
-    proBarArea->addWidget(progress,0,Qt::AlignCenter);
-    proBarArea->addWidget(proBar,0,Qt::AlignCenter);
+    speed->setTextSize(16);
+    QHBoxLayout* upBarArea = new QHBoxLayout();
+    upBarArea->addWidget(speed,0,Qt::AlignCenter);
+    upBarArea->addWidget(progress,0,Qt::AlignCenter);
+    QVBoxLayout* proBarArea = new QVBoxLayout();
+    proBarArea->addLayout(upBarArea);
+    proBarArea->addWidget(proBar, 0, Qt::AlignCenter);
     proBarArea->setAlignment(Qt::AlignCenter);
 
-    //modifyBtn=new ElaIconButton(ElaIconType::CalendarLinesPen);
-    pauseBtn=new ElaIconButton(ElaIconType::CirclePause,20);
-    relieveBtn=new ElaIconButton(ElaIconType::CircleXmark,20);
-    //modifyBtn->setFixedSize(30,30);
-    pauseBtn->setFixedSize(30,30);
-    relieveBtn->setFixedSize(30,30);
+    pauseBtn = new ElaIconButton(ElaIconType::CirclePause, 20);
+    relieveBtn = new ElaIconButton(ElaIconType::CircleXmark, 20);
+    pauseBtn->setFixedSize(30, 30);
+    relieveBtn->setFixedSize(30, 30);
 
-    QHBoxLayout*actionArea=new QHBoxLayout();
-    //actionArea->addWidget(modifyBtn);
+    QHBoxLayout* actionArea = new QHBoxLayout();
     actionArea->addWidget(pauseBtn);
     actionArea->addWidget(relieveBtn);
     actionArea->setAlignment(Qt::AlignCenter);
 
-    QHBoxLayout*FileCardArea=new QHBoxLayout(this);
-    FileCardArea->addLayout(checkBoxArea,Qt::AlignCenter);
-    FileCardArea->addLayout(dataSizeArea,Qt::AlignCenter);
-    FileCardArea->addLayout(speedArea,Qt::AlignCenter);
-    FileCardArea->addLayout(proBarArea,Qt::AlignCenter);
-    FileCardArea->addLayout(actionArea,Qt::AlignCenter);
-    FileCardArea->addStretch();
+    QHBoxLayout* FileCardArea = new QHBoxLayout(this);
+    FileCardArea->addLayout(checkBoxArea); // 将_checkBox布局添加到卡片布局中
+    FileCardArea->addLayout(filenameArea); // 将文件名布局添加到卡片布局中，紧跟_checkBox
+    FileCardArea->addLayout(dataSizeArea);
+    FileCardArea->addLayout(proBarArea);
+    FileCardArea->addLayout(actionArea);
+    FileCardArea->setStretchFactor(checkBoxArea, 25);
+    FileCardArea->setStretchFactor(filenameArea, 500);
+    FileCardArea->setStretchFactor(dataSizeArea, 100);
+    FileCardArea->setStretchFactor(proBarArea, 140);
+    FileCardArea->setStretchFactor(actionArea, 60);
 
-    connect(relieveBtn,&ElaIconButton::clicked,this,&FileCard::on_relieveBtn_clicked);
-    connect(pauseBtn,&ElaIconButton::clicked,this,&FileCard::on_pauseBtn_clicked);
+    connect(relieveBtn, &ElaIconButton::clicked, this, &FileCard::on_relieveBtn_clicked);
+    connect(pauseBtn, &ElaIconButton::clicked, this, &FileCard::on_pauseBtn_clicked);
 }
 
 void FileCard::on_relieveBtn_clicked()
