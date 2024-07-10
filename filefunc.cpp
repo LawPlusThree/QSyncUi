4894d161d66a4ac2aae7dad636ae646e0f952f64
Dipper209 <3611691282@qq.com>
2024-07-10 16:10:25 +0800

    读取云文件夹

--- filefunc.cpp
+++ filefunc.cpp
@@ -1,11 +1,12 @@
 #include "filefunc.h"
 
-
 //用多线程遍历本地文件夹
 void Filefunc::readDirectory(const QString &path)
 {
     // 递归读取文件夹和子文件夹
     recursiveRead(path);
+    emit localTotalSize(totalSize);
+    emit upTotalSize(upFileSize);
 }
 
 void Filefunc::recursiveRead(const QString &path)
@@ -19,61 +20,36 @@ void Filefunc::recursiveRead(const QString &path)
             recursiveRead(info.filePath());
         } else {
             // 如果是文件，添加到文件信息列表
-            addSynctask(info.absoluteFilePath());
+            totalSize+=info.size();
+            addSynctask(info);
         }
     }
 }
 
 void Filefunc::readCLoudDirectory(const QString &cloudpath)
 {
-    // 递归读取云文件夹和子文件夹
-    recursiveReadCloud(cloudpath);
-}
-
-void Filefunc::recursiveReadCloud(const QString &cloudpath)
-{
-    // 读取云文件夹
-    QString prefix = cloudpath;
-    if (!prefix.endsWith("/")) {
-        prefix += "/";
-    }
-
-    // 读取云文件夹下的文件和子文件夹
-    preRequest request;
-    request.queryParams["prefix"] = prefix;
-    request.queryParams["delimiter"] = "/";
-    preResponse response = cosclient->listObjects();
-    QDomDocument doc;
-    doc.setContent(response.data);
-    QDomElement root = doc.documentElement();
-    QDomNode node = root.firstChild();
-    while (!node.isNull()) {
-        QDomElement element = node.toElement();
-        if (element.tagName() == "Contents") {
-            // 如果是文件，添加到文件信息列表
-            QString key = element.firstChildElement("Key").text();
-            QString cloudPath = key.mid(prefix.length());
-            QString localPath = task->getLocalPath() + "/" + cloudPath;
-            addSynctask(localPath);
-        } else if (element.tagName() == "CommonPrefixes") {
-            // 如果是子文件夹，递归读取
-            QString subPrefix = element.firstChildElement("Prefix").text();
-            recursiveReadCloud(subPrefix);
-        }
-        node = node.nextSibling();
-    }
+    XmlProcesser processer;
+    QString xml;
+    Bucket bucket;
+    bucket.isTruncated=false;
+    bucket.nextMarker="";
+    do{
+        xml=cosclient->listObjects(cloudpath,bucket.nextMarker);
+        bucket=processer.processXml(xml);
+    }while(!bucket.isTruncated);
 
 }
 
-void Filefunc::addSynctask(const QString &path)
+void Filefunc::addSynctask(const QFileInfo &info)
 {
     //把path前面和task->getLocalPath()相同的部分去掉
+    QString path=info.absoluteFilePath();
     QString relativePath=path.mid(task->getLocalPath().length()+1);
     QString cloudPath=task->getRemotePath()+relativePath;
     headHeader tmpHeaders;
     preResponse response=cosclient->headObject(cloudPath,"",tmpHeaders);
     if(!cosclient->isExist(response)){
-        qDebug()<<"正在同步:"<<path;
+        upFileSize+=info.size();
         cosclient->putLocalObject(cloudPath,path);
     }
 }
--- filefunc.h
+++ filefunc.h
@@ -4,6 +4,7 @@
 #include "qthread.h"
 #include "synctask.h"
 #include "cosclient.h"
+#include "xmlprocesser.h"
 #include <QObject>
 #include <QDir>
 #include <QFileInfoList>
@@ -18,6 +19,9 @@ public:
     QString path;
     COSClient* cosclient;
     SyncTask* task;
+    qint64 totalSize=0;
+    qint64 upFileSize=0;
+    qint64 downFileSize=0;
     Filefunc(QString pathi,COSClient* cosclienti,SyncTask* taski):path(pathi),cosclient(cosclienti),task(taski){};
     void run() override
     {
@@ -26,11 +30,13 @@ public:
     void readDirectory(const QString &path);
     void recursiveRead(const QString &path);
     void readCLoudDirectory(const QString &cloudpath);
-    void recursiveReadCloud(const QString &cloudpath);
 
-    void addSynctask(const QString &path);//添加同步任务，把本地未上传文件添加入任务
+    void addSynctask(const QFileInfo &info);//添加同步任务，把本地未上传文件添加入任务
     void deleteSynctask(const QString &path);//删除同步任务，把本地已上传文件删除任务
     void updateSynctask(const QString &path);//更新同步任务，把本地已上传文件更新任务
+signals:
+    void localTotalSize(qint64 totalSize);//本地文件总大小
+    void upTotalSize(qint64 upFileSize);//上传文件总大小
 };
 
 
