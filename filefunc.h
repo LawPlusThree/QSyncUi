2b37b608d7cc1728d60d92a4522a92f72ff87ba5
Dipper209 <3611691282@qq.com>
2024-07-10 11:34:45 +0800

    filefunc写同步逻辑

--- filefunc.h
+++ filefunc.h
@@ -2,6 +2,8 @@
 #define FILEFUNC_H
 
 #include "qthread.h"
+#include "synctask.h"
+#include "cosclient.h"
 #include <QObject>
 #include <QDir>
 #include <QFileInfoList>
@@ -14,14 +16,19 @@ class Filefunc : public QThread
 
 public:
     QString path;
-    Filefunc(QString path):path(path){};
+    COSClient* cosclient;
+    SyncTask* task;
+    Filefunc(QString pathi,COSClient* cosclienti,SyncTask* taski):path(pathi),cosclient(cosclienti),task(taski){};
     void run() override
     {
         readDirectory(path);
     }
     void readDirectory(const QString &path);
     void recursiveRead(const QString &path);
-    QFileInfoList fileInfoList;
+
+    void addSynctask(const QString &path);//添加同步任务，把本地未上传文件添加入任务
+    void deleteSynctask(const QString &path);//删除同步任务，把本地已上传文件删除任务
+    void updateSynctask(const QString &path);//更新同步任务，把本地已上传文件更新任务
 };
 
 
