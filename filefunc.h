40e4368c0cc7afba7dcc4b6707c560e91db39c48
Alexander-Porter <alexanderporter@126.com>
2024-07-10 10:20:08 +0800

    在子线程中遍历文件

--- filefunc.h
+++ filefunc.h
@@ -1,6 +1,7 @@
 #ifndef FILEFUNC_H
 #define FILEFUNC_H
 
+#include "qthread.h"
 #include <QObject>
 #include <QDir>
 #include <QFileInfoList>
@@ -8,32 +9,19 @@
 #include <QDateTime>
 #include <QString>
 
-class Filefunc : public QObject
+class Filefunc : public QThread
 {
-    Q_OBJECT
 
 public:
-    explicit Filefunc(QObject *parent = nullptr) : QObject(parent) {}
-
-public slots:
-    // 读取文件夹及其子文件夹中的所有文件
+    QString path;
+    Filefunc(QString path):path(path){};
+    void run() override
+    {
+        readDirectory(path);
+    }
     void readDirectory(const QString &path);
-
-private slots:
-    // 递归遍历文件夹和子文件夹
     void recursiveRead(const QString &path);
-
-private:
-    // 存储文件信息列表
     QFileInfoList fileInfoList;
-
-signals:
-    void fileListUpdated(const QString &path, const QFileInfoList &list);
-
-public:
-    // 获取文件信息列表
-    QFileInfoList getFileInfoList() const { return fileInfoList; }
-    bool comparetime(QDateTime localtime,QString cloudtimestr);//比较两端文件时间,若本地文件时间新则返回真
 };
 
 
