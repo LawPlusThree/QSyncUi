06c0f585eb294e41771fea58a8fca44160ee2f9c
Firefly-29710 <2235501805@qq.com>
2024-07-12 09:49:38 +0800

    历史同步界面

---
+++ historycardproxy.cpp
@@ -0,0 +1,31 @@
+#include "historycardproxy.h"
+#include"historycard.h"
+#include<QVBoxLayout>
+
+HistoryCardProxy::HistoryCardProxy(QWidget *parent) : QWidget(parent) {
+    parentWidget = qobject_cast<QWidget*>(parent);
+    filesLayout=new QVBoxLayout(this);
+}
+
+HistoryCardProxy::~HistoryCardProxy() {
+    cardVector.clear();
+}
+
+void HistoryCardProxy::addHistoryCard(HistoryCard *card) {
+    if (card && parentWidget) {
+        cardVector.push_back(card);
+        filesLayout->addWidget(card);
+        filesLayout->setAlignment(Qt::AlignTop);
+    }
+}
+
+void HistoryCardProxy::addHistoryCard(QString filename,QString datasize,QString time,bool upif)
+{
+    HistoryCard*card=new HistoryCard(filename,datasize,time,upif);
+    if (card && parentWidget) {
+        cardVector.push_back(card);
+        filesLayout->addWidget(card);
+        filesLayout->setAlignment(Qt::AlignTop);
+    }
+}
+
