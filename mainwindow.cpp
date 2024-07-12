303d0bfd5f1f6ec4850a466be5e6a321bf32d277
kuiling <3110428854@qq.com>
2024-07-12 16:30:23 +0800

    重新了注销二次确认界面的ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -118,15 +118,24 @@ MainWindow::MainWindow(QWidget *parent)
                     dialag->setCentralWidget(_centralWidget);
                     dialag->setLeftButtonText("取消");
                     dialag->setRightButtonText("确认");
-                    connect(dialag, &ElaContentDialog::rightButtonClicked, [=]() {
-                        if (cancelcheckBox->isChecked())
+                    dialag->getButton()->setEnabled(false);
+                    dialag->getButton()->setStyleSheet("QPushButton { background-color: #B58C8C; color: white; }");
+                    connect(cancelcheckBox, &ElaCheckBox::stateChanged, [=](int state) {
+                        if (state == Qt::Checked)
                         {
-                            onUserdelete();
+                            dialag->getButton()->setStyleSheet("QPushButton { background-color: #FF6B5D; }"
+                                                               "QPushButton:hover { background-color: red; }"
+                                                               "QPushButton:pressed { background-color: red; }");
+                            dialag->getButton()->setEnabled(true);
                         }
                         else
                         {
-                            onMessage("确认框未勾选，若要注销账号请先勾选！","Warning");
+                            dialag->getButton()->setStyleSheet("QPushButton { background-color: #B58C8C; }");
+                            dialag->getButton()->setEnabled(false);
                         }
+                    });
+                    connect(dialag, &ElaContentDialog::rightButtonClicked, [=]() {
+                        onUserdelete();
                     });
                     dialag->show();
                     }
